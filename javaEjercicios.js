325b6c78ff5ff6cd37bb1d8df2cee90d51b0e978
AbrilUnchalo <abrilunchalo@hotmail.com>
2023-07-18 11:01:03 -0300

    data collection functions, almost

--- javaEjercicios.js
+++ javaEjercicios.js
@@ -1,7 +1,7 @@
 // array to load products
 let productsArray = [];
 
-// class for create news products 
+// class for create news products (in the form of an object)
 class Product {
     constructor(name, quiantityStarting, quantitySold, quantitySupplied) {
         this.name = name;
@@ -17,3 +17,19 @@ class Product {
 calculateEndingStock() {
     this.finalStock = this.quiantityStarting + this.quantitySupplied - this.quantitySold;
   } ;
+
+// Show results of the products and details
+showResult() {
+    console.log("Initial stock of " + this.name + ": " + this.quiantityStarting);
+    console.log("Units sold :" + this.quantitySold);
+    console.log("Replenished units of " + this.name + ": " + this.quantitySupplied);
+    console.log("Total stock of " + this.name + ": " + this.finalStock);
+}
+
+//Function to add a product
+function addProduct () {
+   let name = prompt();
+   let quiantityStarting = prompt();
+   let quantitySold = prompt();
+   let quantitySupplied = prompt();
+}
